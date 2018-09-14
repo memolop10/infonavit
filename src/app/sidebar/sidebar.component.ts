fa2c78cbe663ad6908aa71311de9df535b08aad9
memolop10 <inglopezvargas@gmail.com>
2018-09-14 09:20:38 -0500

    entrega

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+  title = 'Look jQuery Animation working in action!';
 
   constructor() { }
 
-  ngOnInit() {
-  }
+ 
+    public ngOnInit()
+  {
 
+    
+     function setActive(element){
+      
+      console.log(element)
+      $(".fred-li").removeClass("active")
+      $(element).addClass("active")
+ 
+  }
+  
+  
+  }
 }
