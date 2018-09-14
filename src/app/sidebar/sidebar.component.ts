89385d115756653541a8e6b189367bcd4b876aeb
memolop10 <inglopezvargas@gmail.com>
2018-09-14 09:29:01 -0500

    cambios2

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -7,23 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
-  title = 'Look jQuery Animation working in action!';
+  // title = 'Look jQuery Animation working in action!';
 
   constructor() { }
 
  
-    public ngOnInit()
-  {
-
-    
-    //  function setActive(element){
-      
-    //   console.log(element)
-    //   $(".fred-li").removeClass("active")
-    //   $(element).addClass("active")
- 
-  }
-  
-  
+  ngOnInit() {
   }
 
+}
